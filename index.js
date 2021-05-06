711f80b263702bccfe6e430ce803fb563a791371
Priyanshu <priyanshushukla9801@gmail.com>
2021-05-06 17:10:57 +0530

    industry

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 let results = document.getElementsByClassName("results")[0];
+let industry = [];
+let selected_industries = [];
+let industry_select = document.getElementById("industry-select");
+let selected_industries_tag = document.getElementsByClassName(
+  "selected-industries"
+)[0];
 
+for (var x = 0; x < information.length; x++) {
+  if (industry.includes(information[x].industry)) continue;
+  else if (information[x].industry == "") {
+    console.log(information[x]);
+  } else {
+    industry.push(information[x].industry);
+  }
+}
+var option_default = document.createElement("option");
+option_default.text = "Industry :";
+option_default.value = "";
+industry_select.add(option_default);
+
+for (var x = 0; x < industry.length; x++) {
+  var option = document.createElement("option");
+  option.text = industry[x];
+  option.value = industry[x];
+  industry_select.add(option);
+}
 function submit() {
+  results.innerHTML = "";
+
   let branch = document.getElementById("branch-input");
   let info = [];
-  console.log(information);
   for (var i = 0; i < information.length; i++) {
-    console.log(information[i]);
-    if (information[i].projects)
+    let selected_allow =
+      selected_industries.includes(information[i].industry) ||
+      selected_industries.length == 0;
+    if (information[i].projects && selected_allow) {
       for (var j = 0; j < information[i].projects.length; j++) {
         let any = "any";
         let any_index = information[i].projects[
@@ -20,17 +48,26 @@ function submit() {
           (item) => branch.value.toLowerCase() === item.toLowerCase()
         );
 
-        if (branch_index != -1 || any_index != -1) {
+        if (
+          branch_index != -1 ||
+          any_index != -1 ||
+          selected_industries.includes(information[i].industry)
+        ) {
           info.push(information[i]);
         }
       }
+    }
   }
   branch.value = "";
   print(info);
 }
 
 function print(info) {
-  results.innerHTML = "";
+  results.style.marginTop = `${
+    document.getElementsByClassName("input")[0].getBoundingClientRect().height +
+    40
+  }px`;
+
   for (var i = 0; i < info.length; i++) {
     var div = document.createElement("div");
     div.classList.add("company");
@@ -38,7 +75,7 @@ function print(info) {
         <div class="company-details">
             <div class="name">${info[i].name}</div>
             <div class="station-id">Station ID- ${info[i].stationId}</div>
-            <div class="company-id">Company ID- ${info[i].companyId}</div>
+            <div class="company-id">Industry- ${info[i].industry}</div>
           </div>
     `;
 
@@ -64,5 +101,23 @@ function print(info) {
         `;
     }
     results.appendChild(div);
+  }
+}
+
+function clear_all() {
+  results.innerHTML = "";
+  selected_industries = [];
+  selected_industries_tag.innerHTML = `Selected Industries -`;
+}
+
+function selected_industry() {
+  let selected = industry_select.value;
+  industry_select.value = "";
+  if (selected_industries.includes(selected)) return;
+  else {
+    selected_industries.push(selected);
+    selected_industries_tag.innerHTML = `Selected Industries - ${selected_industries.join(
+      " , "
+    )} `;
   }
 }
